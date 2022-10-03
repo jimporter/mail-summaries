ed7c70e67d3caf90205b069db81bb5bc98b7ce2e
Jim Porter <itsjimporter@gmail.com>
2022-10-03 13:32:18 -0700

    Copy WindowUtils.jsm into mailsummaries.js to avoid security-related exception

--- src/api/mailsummaries.js
+++ src/api/mailsummaries.js
@@ -9,9 +9,92 @@ const { ExtensionParent } = ChromeUtils.import(
 const extension = ExtensionParent.GlobalManager.getExtension(
   "mailsummaries@mozillamessaging.com"
 );
-const { WindowInjector } = ChromeUtils.import(extension.rootURI.resolve(
-  "api/WindowUtils.jsm"
-));
+
+class WindowInjector {
+  constructor(windowType) {
+    this.windowType = windowType;
+    this._windowMap = new WeakMap();
+    this._listeners = new Map();
+    let enumerator = Services.wm.getEnumerator(null);
+    while (enumerator.hasMoreElements()) {
+      let window = enumerator.getNext();
+      if (this.isWindowRelevant(window))
+        this._windowMap.set(window, new Map());
+    }
+
+    let self = this;
+    this._listener = {
+      onOpenWindow(xulWindow) {
+        self._setupWindow(xulWindow.docShell.domWindow);
+      },
+      onCloseWindow(xulWindow) {
+        self._forgetWindow(xulWindow.docShell.domWindow);
+      },
+      onWindowTitleChange(xulWindow, title) {},
+    };
+    Services.wm.addListener(this._listener);
+  }
+
+  inject(id, setup, cleanup) {
+    this._listeners.set(id, {setup, cleanup});
+
+    let enumerator = Services.wm.getEnumerator(null);
+    while (enumerator.hasMoreElements()) {
+      let window = enumerator.getNext();
+      if (this.isWindowRelevant(window))
+        this._windowMap.get(window).set(id, setup(window));
+    }
+  }
+
+  isWindowRelevant(window) {
+    let windowType = window.document.documentElement.getAttribute("windowtype");
+    return windowType === this.windowType;
+  }
+
+  stop() {
+    let enumerator = Services.wm.getEnumerator(null);
+    while (enumerator.hasMoreElements()) {
+      let window = enumerator.getNext();
+      this._cleanupWindow(window);
+    }
+
+    Services.wm.removeListener(this._listener);
+  }
+
+  _runSetup(window, callback) {
+    if (window.document.readyState == "complete") {
+      callback(window);
+    } else {
+      window.addEventListener("load", function onload() {
+        window.removeEventListener("load", onload);
+        callback(window);
+      });
+    }
+  }
+
+  _setupWindow(window) {
+    this._runSetup(window, (window) => {
+      if (!this.isWindowRelevant(window))
+        return;
+      let result = new Map();
+      for (let [k, v] of this._listeners)
+        result.set(k, v.setup(window));
+      this._windowMap.set(window, result);
+    });
+  }
+
+  _cleanupWindow(window) {
+    if (!this.isWindowRelevant(window))
+      return;
+    for (let [k, v] of this._windowMap.get(window)) {
+      this._listeners.get(k).cleanup(window, v);
+    }
+  }
+
+  _forgetWindow(window) {
+    this._windowMap.delete(window);
+  }
+}
 
 function summarizeFolder(window, messageDisplay) {
   const url = "chrome://mailsummaries/content/folderSummary.xhtml";
